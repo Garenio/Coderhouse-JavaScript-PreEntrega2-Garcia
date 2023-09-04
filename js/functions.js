0bfd45708b4f758f46323084d874915865ab17c7
Eugenio Garcia <eugeniog288@gmail.com>
2023-09-03 22:17:34 -0300

    Se agregan las funciones de filtro de jugadores activos e inactivos

--- js/functions.js
+++ js/functions.js
@@ -11,7 +11,45 @@ function contentMaker(searchTitle, list) {
 }
 
 function characterList() {
-    let searchContent = contentMaker("Listado de personajes: \n\n", characters);
+
+    let searchContent = contentMaker("LISTADO DE PERSONAJES \n\n", characters);
+
+    if (document.getElementById("div-content")) {
+        document.getElementById("div-content").innerHTML = "";
+        setTimeout(() => {
+            document.getElementById("div-content").innerText = searchContent
+        }, 100);
+    } else {
+        let divContent = document.createElement("div");
+        divContent.id = "div-content";
+        divContent.innerText = searchContent
+        document.body.append(divContent);
+    }
+};
+
+function activeCharacterList() {
+
+    let activeCharacters = characters.filter((el) => el.isActive);
+
+    let searchContent = contentMaker("LISTADO DE PERSONAJES ACTIVOS \n\n", activeCharacters);
+
+    if (document.getElementById("div-content")) {
+        document.getElementById("div-content").innerHTML = "";
+        setTimeout(() => {
+            document.getElementById("div-content").innerText = searchContent
+        }, 100);
+    } else {
+        let divContent = document.createElement("div");
+        divContent.id = "div-content";
+        divContent.innerText = searchContent
+        document.body.append(divContent);
+    }
+};
+function inactiveCharacterList() {
+
+    let inactiveCharacters = characters.filter((el) => el.isActive == false);
+
+    let searchContent = contentMaker("LISTADO DE PERSONAJES INACTIVOS \n\n", inactiveCharacters);
 
     if (document.getElementById("div-content")) {
         document.getElementById("div-content").innerHTML = "";
@@ -30,6 +68,10 @@ function choise(btnValue) {
     switch (btnValue) {
         case 1:
             return characterList();
+        case 2:
+            return activeCharacterList();     
+        case 3:
+            return inactiveCharacterList();     
         default:
             break;
     }
